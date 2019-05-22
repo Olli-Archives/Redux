a6dd0f7919c7947721c1cc0e3858dd454e075485
ollinieminen <otn987@gmail.com>
2019-05-22 14:56:28 -0700

    done

--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
 import { createStore } from 'redux';
 
 const initialState = {
-  stuff: 'unfinished'
+  beer: null,
+  sandwich: null,
+  chips: null
 };
 
 function reducer(state = initialState, action) {
   switch(action.type) {
-    case: 'DO_STUFF':
-      return { stuff: 'done' };
+    case 'ADD_BEER':
+      return { beer: action.payload };
+    case 'REMOVE_BEER':
+      return { beer: null };
+    case 'ADD_SWANDWICH':
+      return { sandwich: action.payload };
+    case 'REMOVE_SANDWICH':
+      return { sandwich: null };
+    case 'ADD_CHIPS':
+      return { chips: action.payload };
+    case 'REMOVE_CHIPS':
+      return { chips: null };
+
     default:
       return state;
   }
 }
 
 const store = createStore(reducer);
+
+store.dispatch({
+  type: 'ADD_BEER',
+  payload: 'Pale Ale'
+});
+console.log('added drink', store.getState());
+
+store.dispatch({
+  type: 'REMOVE_BEER',
+  payload: 'Pale Ale'
+});
+console.log('removed drink', store.getState());
+store.dispatch({
+  type: 'ADD_CHIPS',
+  payload: 'Salt and Vinegar'
+});
+console.log('add chips', store.getState());
+store.dispatch({
+  type: 'REMOVE_CHIPS',
+  payload: 'Salt and Vinegar'
+});
+console.log('remove chips', store.getState());
+store.dispatch({
+  type: 'ADD_SWANDWICH',
+  payload: 'PBJ'
+});
+console.log('add sandwich', store.getState());
+store.dispatch({
+  type: 'REMOVE_SANDWICH',
+  payload: 'SPBJ'
+});
+console.log('remove sandwich', store.getState());
